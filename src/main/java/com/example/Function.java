66b58bce6b016c3c1c0b0b735e5b780ad7acc756
tomasstorc <tomas.storc@gmail.com>
2023-09-22 10:26:15 +0200

    feat: test13 (#23)
    Co-authored-by: tstorc-cen68444 <tstorc@csas.cz>

--- src/main/java/com/example/Function.java
+++ src/main/java/com/example/Function.java
@@ -28,7 +28,7 @@ public class Function {
                 authLevel = AuthorizationLevel.ANONYMOUS)
                 HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed a request. Test12");
+        context.getLogger().info("Java HTTP trigger processed a request. Test13");
 
         // Parse query parameter
         final String query = request.getQueryParameters().get("name");
